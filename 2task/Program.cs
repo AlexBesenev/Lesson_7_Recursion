e31a45f9077e21d413676d6fa0fecd45098edecc
Alex Besenev <Besenev96@gmail.com>
2024-03-04 23:28:26 +0500

    add 2 and 3 task

---
+++ 2task/Program.cs
@@ -0,0 +1,26 @@
+﻿/***********************************************
+Задача 2: Напишите программу вычисления функции
+Аккермана с помощью рекурсии. Даны два
+неотрицательных числа m и n. 
+m = 2, n = 3 -> A(m,n) = 29 
+(Ошибка в примере, по таблице из различных источников при данных значениях функция A(m,n) = 9, а не 29!)
+************************************************/
+
+int CalculateAckermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0)
+        {
+            return CalculateAckermann(m - 1, 1);
+        }
+    }
+    return CalculateAckermann(m - 1, CalculateAckermann(m, n - 1));
+}
+int m = 3;
+int n = 2;
+System.Console.WriteLine(CalculateAckermann(m, n));
---
+++ 3task/Program.cs
@@ -0,0 +1,27 @@
+﻿/**************************************
+ Задайте произвольный массив. Выведете
+его элементы, начиная с конца. Использовать
+рекурсию, не использовать циклы.
+пример:
+[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+***************************************/
+
+void OutputArrayFromEnd(int[] array, int i = 0)
+{
+    if (i < array.Length)
+    {
+        OutputArrayFromEnd(array, i + 1);
+        if (i > 0)
+        {
+            System.Console.Write(array[i] + " ");
+        }
+        else
+        {
+            System.Console.WriteLine(array[i]);
+        }
+    }
+    return;
+}
+
+int[] auf = { 1, 2, 5, 0, 10, 34 };
+OutputArrayFromEnd(auf);
